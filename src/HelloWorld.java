c40d63c80f2f9389227344f57be28b3703984532
codenameashley <ashleynavo@googlemail.com>
2022-05-04 14:57:10 +0100

    Type Casting & Primitives Types Challenge

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,31 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Ashley");
 
+        int myValue = 1000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
         int myThirdNumber = myFirstNumber * 2;
@@ -10,6 +35,18 @@ public class HelloWorld {
 
         System.out.println(myTotal);
 
+        // Type Casting
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        short myNewShortValue = (short) (myMinShortValue / 2);
 
+        // Primitives Types Challenge
+        byte myByteVariable = 100;
+        short myShortVariable = 1_000;
+        int myIntegerVariable = 10_000;
+        long myLongVariable = (5000L + (10L * (myByteVariable + myShortVariable + myIntegerVariable)));
+        System.out.println(myLongVariable);
+
+        short shortTotal = (short) (1000 + 10 * (myByteVariable + myShortVariable + myIntegerVariable));
+        System.out.println(shortTotal);
     }
 }
