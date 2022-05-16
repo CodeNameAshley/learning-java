afdae654e14597a1616766cf4ce579392946b477
Ashley <ashleynavo@googlemail.com>
2022-05-16 22:07:39 +0100

    Added Tree.java to the project.

---
+++ src/Tree.java
@@ -0,0 +1,18 @@
+public class Tree {
+    public static void main(String[] args) {
+        //This method will introduce myself and print a tree to the screen.
+        System.out.println("Hey there, I'm Ashley and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!"
+        );
+
+        System.out.println("Ready to get my hands dirty"
+        );
+
+        System.out.println("  *****  ");
+        System.out.println("  *****  ");
+        System.out.println("  *****  ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println(" ####### ");
+    }
+}
